f2ef6c8d1c80b6ca6a769ab0a3cfccbcf8dfefc4
thexdev <akbarnugroho253@gmail.com>
2020-01-22 19:04:24 +0700

    create service data

---
+++ src/blocks/services/services-data.js
@@ -0,0 +1,91 @@
+import gem from "assets/images/icon/gem.svg";
+import cookie from "assets/images/icon/cookie.svg";
+import cog from "assets/images/icon/cog.svg";
+import thunder from "assets/images/icon/thunder.svg";
+import cart from "assets/images/icon/cart.svg";
+import website from "assets/images/icon/website.svg";
+import wrench from "assets/images/icon/wrench.svg";
+import equaliser from "assets/images/icon/equaliser.svg";
+
+export default [
+  {
+    icon: {
+      name: gem,
+      alternative: "gem icon"
+    },
+    content: {
+      title: "slicing mockup",
+      description: "lorem ipsum dolor sit amet"
+    }
+  },
+  {
+    icon: {
+      name: cookie,
+      alternative: "cookie icon"
+    },
+    content: {
+      title: "redesign web",
+      description: "lorem ipsum dolor sit amet"
+    }
+  },
+  {
+    icon: {
+      name: cog,
+      alternative: "gear icon"
+    },
+    content: {
+      title: "microservice",
+      description: "lorem ipsum dolor sit amet"
+    }
+  },
+  {
+    icon: {
+      name: thunder,
+      alternative: "thunder icon"
+    },
+    content: {
+      title: "rest api",
+      description: "lorem ipsum dolor sit amet"
+    }
+  },
+  {
+    icon: {
+      name: cart,
+      alternative: "ecommers icon"
+    },
+    content: {
+      title: "e-commers",
+      description: "lorem ipsum dolor sit amet"
+    }
+  },
+  {
+    icon: {
+      name: website,
+      alternative: "website icon"
+    },
+    content: {
+      title: "blog",
+      description: "lorem ipsum dolor sit amet"
+    }
+  },
+  {
+    icon: {
+      name: equaliser,
+      alternative: "setup icon"
+    },
+    content: {
+      title: "setup vps",
+      description: "lorem ipsum dolor sit amet"
+    }
+  },
+  {
+    icon: {
+      name: wrench,
+      alternative: "wrench icon"
+    },
+    content: {
+      title: "maintenance",
+      description: "lorem ipsum dolor sit amet"
+    }
+  }
+];
