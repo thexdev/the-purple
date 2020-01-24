4f734bc5d5b369535b6f1de519b93fd0fcd742e3
thexdev <akbarnugroho253@gmail.com>
2020-01-24 22:44:46 +0700

    create block skillset image

---
+++ src/blocks/skillset/skillset-image.js
@@ -0,0 +1,18 @@
+import React, { memo } from "react";
+import { Col } from "react-bootstrap";
+import skillset from "assets/images/about/skillset.svg";
+
+function SkillsetImage() {
+  return (
+    <Col md={4}>
+      <h3 className="text-uppercase text-center">Tech Stack</h3>
+      <img
+        src={skillset}
+        alt="skillset"
+        className="img-fluid shadow rounded border d-block"
+      />
+    </Col>
+  );
+}
+
+export default memo(SkillsetImage);
