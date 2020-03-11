48db9166a3f823c5b25dd2e13c9d299b3ed45391
thexdev <akbarnugroho253@gmail.com>
2020-03-11 22:48:52 +0700

    changed older section layout to section component

--- src/blocks/about/index.js
+++ src/blocks/about/index.js
@@ -1,32 +1,65 @@
-import React, { useState, lazy, memo } from "react";
+import React, { memo } from "react";
+import { Col } from "react-bootstrap";
+import Section from "components/section";
+import Skillset from "blocks/skillset";
 import vr from "assets/images/icon/vr.svg";
-import aboutContents from "./about-data";
 
-const SectionGrid3 = lazy(() =>
-  import("components/section-grid/section-grid-3")
-);
-
-const Skillset = lazy(() => import("blocks/skillset"));
-
-function About() {
-  const [icon] = useState({
+const About = () => {
+  const icon = {
     name: vr,
     alternative: "robot"
-  });
-
-  const [contents] = useState(aboutContents);
+  };
 
   return (
     <>
-      <SectionGrid3
-        heading="about me"
-        subheading="a freak and passionate person"
+      <Section
+        title="about me"
+        subtitle="a freak and passionate person"
         icon={icon}
-        contents={contents}
-      />
+      >
+        <Col xs={12} md={4}>
+          <p className="text-left">
+            So, I'm currently working as a web developer on{" "}
+            <a href="https://rnj.id" target="_blank" rel="noopener noreferrer">
+              JnR Indonesia
+            </a>
+            . This is my first year as a web developer on real world.
+          </p>
+          <p className="text-left">
+            I really interested with latest technology specially on Hybrid App
+            and Blockchain. I'm a fast learner, good attitude and easy going.
+          </p>
+        </Col>
+        <Col xs={12} md={4}>
+          <p className="text-left">
+            I always use modern tools and technology for every project i made.
+            It cover's: <a href="spa">SPA</a>, <a href="pwa">PWA</a>,{" "}
+            <a href="Containerization">Containerization</a> and{" "}
+            <a href="Cloud Computing">Cloud Computing</a>.
+          </p>
+          <p className="text-left">
+            My skill for programming language follow <code>HTML</code>,{" "}
+            <code>CSS</code>, <code>JavaScript</code>, <code>Python</code> and{" "}
+            <code>Dart</code>. Also i have enough experience on <code>PHP</code>{" "}
+            (ex <code>PHP</code> dev, lol).
+          </p>
+        </Col>
+        <Col xs={12} md={4}>
+          <p className="text-left">
+            I'm able to operate some OS, such <a href="linux">Ubuntu</a>,{" "}
+            <a href="windows">Windows 7, 8, 10</a> and <a href="osx">OSX</a>.
+            I'm able to write documentation of code carefuly.
+          </p>
+          <p className="text-left">
+            I love to work in orgenized and solve problem with team together. My
+            team will really love me because i have a lot of free ebook for
+            them.
+          </p>
+        </Col>
+      </Section>
       <Skillset />
     </>
   );
-}
+};
 
 export default memo(About);
