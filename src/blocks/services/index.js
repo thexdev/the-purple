7565dad8348a192fbcfa63c3b29a0fecd6221215
thexdev <akbarnugroho253@gmail.com>
2020-03-11 22:59:40 +0700

    replaced SectionGridBox4 with Section component

--- src/blocks/services/index.js
+++ src/blocks/services/index.js
@@ -1,27 +1,120 @@
-import React, { useState, lazy, memo } from "react";
+import React, { useState, memo } from "react";
+import { Col } from "react-bootstrap";
+import Section from "components/section";
+import Box from "components/box";
+
 import layers from "assets/images/icon/layers.svg";
-import ServicesData from "./services-data";
+import gem from "assets/images/icon/gem.svg";
+import cookie from "assets/images/icon/cookie.svg";
+import cog from "assets/images/icon/cog.svg";
+import thunder from "assets/images/icon/thunder.svg";
+import cart from "assets/images/icon/cart.svg";
+import website from "assets/images/icon/website.svg";
+import wrench from "assets/images/icon/wrench.svg";
+import equaliser from "assets/images/icon/equaliser.svg";
 
-const SectionGridBox4 = lazy(() =>
-  import("components/section-grid/section-grid-box-4")
-);
+const Services = () => {
+  const [services] = useState([
+    {
+      icon: {
+        name: gem,
+        alternative: "gem icon"
+      },
+      content: {
+        title: "slicing mockup",
+        description: "lorem ipsum dolor sit amet"
+      }
+    },
+    {
+      icon: {
+        name: cookie,
+        alternative: "cookie icon"
+      },
+      content: {
+        title: "redesign web",
+        description: "lorem ipsum dolor sit amet"
+      }
+    },
+    {
+      icon: {
+        name: cog,
+        alternative: "gear icon"
+      },
+      content: {
+        title: "microservice",
+        description: "lorem ipsum dolor sit amet"
+      }
+    },
+    {
+      icon: {
+        name: thunder,
+        alternative: "thunder icon"
+      },
+      content: {
+        title: "rest api",
+        description: "lorem ipsum dolor sit amet"
+      }
+    },
+    {
+      icon: {
+        name: cart,
+        alternative: "ecommers icon"
+      },
+      content: {
+        title: "e-commers",
+        description: "lorem ipsum dolor sit amet"
+      }
+    },
+    {
+      icon: {
+        name: website,
+        alternative: "website icon"
+      },
+      content: {
+        title: "blog",
+        description: "lorem ipsum dolor sit amet"
+      }
+    },
+    {
+      icon: {
+        name: equaliser,
+        alternative: "setup icon"
+      },
+      content: {
+        title: "setup vps",
+        description: "lorem ipsum dolor sit amet"
+      }
+    },
+    {
+      icon: {
+        name: wrench,
+        alternative: "wrench icon"
+      },
+      content: {
+        title: "maintenance",
+        description: "lorem ipsum dolor sit amet"
+      }
+    }
+  ]);
 
-function Services() {
-  const [icon] = useState({
+  const icon = {
     name: layers,
     alternative: "layers icon"
-  });
-
-  const [contents] = useState(ServicesData);
+  };
 
   return (
-    <SectionGridBox4
-      heading="services"
-      subheading="lorem ipsum dolor sit amet"
+    <Section
+      title="services"
+      subtitle="choose what you need, i'll create that"
       icon={icon}
-      contents={contents}
-    />
+    >
+      {services.map((service, index) => (
+        <Col key={index} xs={12} sm={6} md={3}>
+          <Box icon={service.icon} content={service.content} />
+        </Col>
+      ))}
+    </Section>
   );
-}
+};
 
 export default memo(Services);
