3e33c05cb34b5cc90eb4212f02b02da785dc0a80
thexdev <akbarnugroho253@gmail.com>
2020-03-11 22:35:43 +0700

    added routes, home page, about page, service page, portfolio page, blog page, contact page, not found page

--- src/App.js
+++ src/App.js
@@ -1,32 +1,28 @@
 import React, { lazy, Suspense, memo } from "react";
+import { Switch, Route } from "react-router-dom";
 
-const Navbar = lazy(() => import("components/navigation"));
-const Hero = lazy(() => import("blocks/hero"));
-const About = lazy(() => import("blocks/about"));
-const Service = lazy(() => import("blocks/services"));
-const Portfolio = lazy(() => import("blocks/portfolio"));
-const Github = lazy(() => import("blocks/github"));
-const Blog = lazy(() => import("blocks/blog"));
+const Home = lazy(() => import("pages/home"));
+const About = lazy(() => import("pages/about"));
+const Service = lazy(() => import("pages/service"));
+const Portfolio = lazy(() => import("pages/portfolio"));
+const Blog = lazy(() => import("pages/blog"));
+const Contact = lazy(() => import("pages/contact"));
+const NotFound = lazy(() => import("pages/404"));
 
-function App() {
+const App = () => {
   return (
     <Suspense fallback="loading...">
-      <main>
-        <Navbar />
-        <Hero />
-        <About />
-        <hr />
-        <Service />
-        <hr />
-        <Portfolio />
-        <hr />
-        <Github />
-        <hr />
-        <Blog />
-        <hr />
-      </main>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/about" component={About} />
+        <Route path="/services" component={Service} />
+        <Route path="/portfolio" component={Portfolio} />
+        <Route path="/blog" component={Blog} />
+        <Route path="/contact" component={Contact} />
+        <Route component={NotFound} />
+      </Switch>
     </Suspense>
   );
-}
+};
 
 export default memo(App);
