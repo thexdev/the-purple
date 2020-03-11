07e973026bd67408e279805eb25f07e046cb809e
thexdev <akbarnugroho253@gmail.com>
2020-03-11 23:08:32 +0700

    added react moment, blog content frame

--- src/blocks/blog/blog-article.js
+++ src/blocks/blog/blog-article.js
@@ -1,13 +1,25 @@
 import React, { memo } from "react";
 import { Card } from "react-bootstrap";
+import Moment from "react-moment";
 
-function BlogArticle() {
+function BlogArticle(props) {
   return (
     <Card>
-      <Card.Img src="https://placehold.it/500x250" />
-      <Card.Body>
-        <Card.Title>Akbar</Card.Title>
-      </Card.Body>
+      <Card.Link href={props.url} className="text-dark">
+        <Card.Img src={props.cover} />
+        <Card.Body>
+          <small className="text-secondary">
+            <Moment format="LLLL">{props.date}</Moment>
+          </small>
+          <Card.Title>{props.title}</Card.Title>
+          <Card.Text>{props.description}</Card.Text>
+          {props.tags.map((tag, index) => (
+            <span className="badge badge-danger badge-pill mr-1" key={index}>
+              {tag}
+            </span>
+          ))}
+        </Card.Body>
+      </Card.Link>
     </Card>
   );
 }
