9c1cbbc6af17a068639b436147d5fcaeb83fc0a0
thexdev <akbarnugroho253@gmail.com>
2020-03-11 23:06:10 +0700

    replaced SectionCustom with Section component, fixed memory leak when fetch repos from github

--- src/blocks/github/index.js
+++ src/blocks/github/index.js
@@ -1,54 +1,49 @@
-import React, { useState, useEffect, lazy, memo } from "react";
+import React, { useState, useEffect, memo } from "react";
+import { Col } from "react-bootstrap";
+import Section from "components/section";
+import GithubBox from "components/github-box";
+
 import axios from "axios";
-import { Row, Col } from "react-bootstrap";
+import StrHelper from "helpers/string";
 import doc from "assets/images/icon/doc.svg";
 
-const SectionCustom = lazy(() => import("components/section-custom/"));
-const GithubBox = lazy(() => import("components/github-box"));
-
-function Github() {
-  const [icon] = useState({
-    name: doc,
-    alternative: "document icon"
-  });
-
+const Github = () => {
   const [repos, setRepos] = useState([]);
 
-  const stripHyphens = str => str.split("-").join(" ");
-
-  let githubApi = `https://api.github.com/users/thexdev/repos`;
+  const icon = {
+    name: doc,
+    alternative: "document icon"
+  };
 
   useEffect(() => {
     axios
-      .get(githubApi)
+      .get("https://api.github.com/users/thexdev/repos")
       .then(response => setRepos(response.data))
       .catch(console.error);
-  }, [repos, githubApi]);
+  }, []);
 
   return (
-    <SectionCustom
-      heading="repository"
-      subheading="lorem ipsum dolor sit amet"
+    <Section
+      title="repository"
+      subtitle="i'm an open source person, fork it all"
       icon={icon}
     >
-      <Row>
-        {repos.map(repo => (
-          <Col key={repo.id} xs={12} md={3}>
-            <GithubBox
-              image={{
-                name: repo.owner.avatar_url,
-                aleternative: "M. Akbar Nugroho"
-              }}
-              content={{
-                title: stripHyphens(repo.name),
-                description: repo.description
-              }}
-            />
-          </Col>
-        ))}
-      </Row>
-    </SectionCustom>
+      {repos.map(repo => (
+        <Col key={repo.id} xs={12} md={3}>
+          <GithubBox
+            image={{
+              name: repo.owner.avatar_url,
+              aleternative: "M. Akbar Nugroho"
+            }}
+            content={{
+              title: StrHelper.stripHypens(repo.name),
+              description: repo.description
+            }}
+          />
+        </Col>
+      ))}
+    </Section>
   );
-}
+};
 
 export default memo(Github);
