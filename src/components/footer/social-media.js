b29a3739ec100291a3140a85b2ab9a0ef51967e0
thexdev <akbarnugroho253@gmail.com>
2020-04-11 22:10:53 +0700

    added social media links

--- src/components/footer/social-media.js
+++ src/components/footer/social-media.js
@@ -6,17 +6,32 @@ const SocialMedia = () => {
       <h5 className="mb-3">Social</h5>
       <ul className="list-unstyled">
         <li className="my-1">
-          <a href="" className="text-decoration-none text-secondary">
+          <a
+            href="https://www.facebook.com/thexdev"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-decoration-none text-secondary"
+          >
             Facebook
           </a>
         </li>
         <li className="my-1">
-          <a href="" className="text-decoration-none text-secondary">
+          <a
+            href="https://twitter.com/thexdev"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-decoration-none text-secondary"
+          >
             Twitter
           </a>
         </li>
         <li className="my-1">
-          <a href="" className="text-decoration-none text-secondary">
+          <a
+            href="https://medium.com/@thexdev"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-decoration-none text-secondary"
+          >
             Medium
           </a>
         </li>
