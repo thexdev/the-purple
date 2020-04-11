970926b7150c43169e6f007c042a245768eca15d
thexdev <akbarnugroho253@gmail.com>
2020-04-11 17:28:23 +0700

    added "baclground-color: #2d2b55;" to jumbotron component

--- src/components/layout/layout-with-header.js
+++ src/components/layout/layout-with-header.js
@@ -3,24 +3,27 @@ import { Jumbotron } from "react-bootstrap";
 
 const Navigation = lazy(() => import("components/navigation"));
 
-const LayoutWithHeader = props => {
+const LayoutWithHeader = (props) => {
+  const style = { backgroundColor: "#2d2b55" };
   return (
     <main>
       <Navigation />
       <header>
-        <Jumbotron fluid className="mt-md-5">
-          <h1 className="text-center text-white">{props.title}</h1>
-          <p className="text-center text-capitalize lead">{props.subtitle}</p>
+        <Jumbotron fluid style={style} className="mb-0 pb-0">
+          <h1 className="text-center text-white pt-3 pt-md-5">{props.title}</h1>
+          <p className="text-center text-white-50 text-capitalize lead">
+            {props.subtitle}
+          </p>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path
+              fill="#f5f5f6"
+              fillOpacity="1"
+              d="M0,128L40,112C80,96,160,64,240,85.3C320,107,400,181,480,186.7C560,192,640,128,720,117.3C800,107,880,149,960,165.3C1040,181,1120,171,1200,149.3C1280,128,1360,96,1400,80L1440,64L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"
+            ></path>
+          </svg>
         </Jumbotron>
       </header>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-        <path
-          fill="#2d2b55"
-          fillOpacity="1"
-          d="M0,160L34.3,149.3C68.6,139,137,117,206,106.7C274.3,96,343,96,411,122.7C480,149,549,203,617,202.7C685.7,203,754,149,823,144C891.4,139,960,181,1029,186.7C1097.1,192,1166,160,1234,144C1302.9,128,1371,128,1406,128L1440,128L1440,0L1405.7,0C1371.4,0,1303,0,1234,0C1165.7,0,1097,0,1029,0C960,0,891,0,823,0C754.3,0,686,0,617,0C548.6,0,480,0,411,0C342.9,0,274,0,206,0C137.1,0,69,0,34,0L0,0Z"
-        ></path>
-      </svg>
-      {props.children}
+      <section style={{ backgroundColor: "#f5f5f6" }}>{props.children}</section>
     </main>
   );
 };
