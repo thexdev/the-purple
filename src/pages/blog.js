5e672ee23c42cb2f1ecc84e3de45cd2b750bfb57
thexdev <akbarnugroho253@gmail.com>
2020-04-11 17:31:20 +0700

    added "Wrapper", "Article", "NoArticle" component

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,18 +1,45 @@
-import React, { lazy, memo } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React, { useEffect, useState, memo } from "react";
+import { Container, Row } from "react-bootstrap";
 
-const LayoutWithHeader = lazy(() =>
-  import("components/layout/layout-with-header")
-);
+import Wrapper from "blocks/blog/blog-wrapper";
+import Article from "blocks/blog/blog-article";
+import NoArticle from "blocks/blog/blog-no-article";
+import LayoutWithHeader from "components/layout/layout-with-header";
+
+import axios from "axios";
 
 const Blog = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://dev.to/api/articles/?username=thexdev")
+      .then(response => setArticles(response.data))
+      .catch(error => console.error(error));
+  }, []);
+
   return (
-    <LayoutWithHeader title="Blog" subtitle="">
+    <LayoutWithHeader title="Blog" subtitle="Share My Knowledge To Everyone">
       <Container>
         <Row>
-          <Col>
-            <p className="text-center">M. Akbar Nugroho</p>
-          </Col>
+          {articles.length > 0 ? (
+            articles.map(article => (
+              <Wrapper key={article.id}>
+                <Article
+                  title={article.title}
+                  description={article.description}
+                  cover={article.cover_image}
+                  date={article.published_at}
+                  tags={article.tag_list}
+                  url={article.url}
+                />
+              </Wrapper>
+            ))
+          ) : (
+            <Wrapper grid={12}>
+              <NoArticle />
+            </Wrapper>
+          )}
         </Row>
       </Container>
     </LayoutWithHeader>
