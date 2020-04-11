538a8a08470cb7bc7ec9f4134d00ee121946721d
thexdev <akbarnugroho253@gmail.com>
2020-04-11 17:26:51 +0700

    added "SocialMedicaFloatBtn", "SocialMedicaFloatBtnMobile"

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,11 +1,19 @@
 import React, { lazy, memo } from "react";
 
 const Navigation = lazy(() => import("components/navigation"));
+const SocialMediaFloatBtn = lazy(() =>
+  import("components/social-media-float-btn")
+);
+const SocialMediaFloatBtnMobile = lazy(() =>
+  import("components/social-media-float-btn/mobile")
+);
 
-const MainLayout = props => {
+const MainLayout = (props) => {
   return (
     <main>
       <Navigation />
+      <SocialMediaFloatBtn />
+      <SocialMediaFloatBtnMobile />
       {props.children}
     </main>
   );
