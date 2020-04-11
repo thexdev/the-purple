b6357a1168ce23d857383bed39bcd9ed8731016e
thexdev <akbarnugroho253@gmail.com>
2020-04-12 00:47:25 +0700

    replaced axios with fetch

--- src/components/contact-form/message-form.js
+++ src/components/contact-form/message-form.js
@@ -47,7 +47,7 @@ const MessageForm = () => {
       dangerMode: true,
     };
 
-    axios("https://thexdev.netlify.com/", {
+    fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: FormHelper.urlencoded({ "form-name": "message", ...formMessage }),
