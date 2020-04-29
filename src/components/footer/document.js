5c7291ec940947101e1685e475588737d0081764
thexdev <akbarnugroho253@gmail.com>
2020-04-30 00:07:55 +0700

    added "cv" link

--- src/components/footer/document.js
+++ src/components/footer/document.js
@@ -1,4 +1,4 @@
-import React, { memo } from "react";
+import React, { memo } from 'react';
 
 const Document = () => {
   return (
@@ -6,12 +6,15 @@ const Document = () => {
       <h5 className="mb-3">Document</h5>
       <ul className="list-unstyled">
         <li className="my-1">
-          <a href="" className="text-decoration-none text-secondary">
+          <a
+            href="https://drive.google.com/file/d/1h54pxCR3Si7N2fngWoBTAKbUrC3HxxUm/view?usp=sharing"
+            className="text-decoration-none text-secondary"
+          >
             Curriculumn Vitae
           </a>
         </li>
         <li className="my-1">
-          <a href="" className="text-decoration-none text-secondary">
+          <a href="/" className="text-decoration-none text-secondary">
             Resume
           </a>
         </li>
