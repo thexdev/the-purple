20b84a34b4d40224e67543448ffd656985f77899
thexdev <akbarnugroho253@gmail.com>
2020-04-30 00:02:24 +0700

    changed content text

--- src/blocks/about/index.js
+++ src/blocks/about/index.js
@@ -1,15 +1,15 @@
-import React, { memo } from "react";
-import { Col } from "react-bootstrap";
-import Section from "components/section";
-import Skillset from "blocks/skillset";
-import vr from "assets/images/icon/vr.svg";
+import React, { memo } from 'react';
+import { Col } from 'react-bootstrap';
+import Section from 'components/section';
+import Skillset from 'blocks/skillset';
+import vr from 'assets/images/icon/vr.svg';
+
+const icon = {
+  name: vr,
+  alternative: 'robot',
+};
 
 const About = () => {
-  const icon = {
-    name: vr,
-    alternative: "robot"
-  };
-
   return (
     <>
       <Section
@@ -19,7 +19,7 @@ const About = () => {
       >
         <Col xs={12} md={4}>
           <p className="text-left">
-            So, I'm currently working as a web developer on{" "}
+            So, I'm currently working as a web developer on{' '}
             <a href="https://rnj.id" target="_blank" rel="noopener noreferrer">
               JnR Indonesia
             </a>
@@ -33,20 +33,19 @@ const About = () => {
         <Col xs={12} md={4}>
           <p className="text-left">
             I always use modern tools and technology for every project i made.
-            It cover's: <a href="spa">SPA</a>, <a href="pwa">PWA</a>,{" "}
-            <a href="Containerization">Containerization</a> and{" "}
+            It cover's: <a href="spa">SPA</a>, <a href="pwa">PWA</a>,{' '}
+            <a href="Containerization">Containerization</a> and{' '}
             <a href="Cloud Computing">Cloud Computing</a>.
           </p>
           <p className="text-left">
-            My skill for programming language follow <code>HTML</code>,{" "}
-            <code>CSS</code>, <code>JavaScript</code>, <code>Python</code> and{" "}
-            <code>Dart</code>. Also i have enough experience on <code>PHP</code>{" "}
-            (ex <code>PHP</code> dev, lol).
+            My skill for programming language follow <code>HTML</code>,{' '}
+            <code>CSS</code>, <code>JavaScript</code>, <code>PHP</code> and{' '}
+            <code>Python</code>.
           </p>
         </Col>
         <Col xs={12} md={4}>
           <p className="text-left">
-            I'm able to operate some OS, such <a href="linux">Ubuntu</a>,{" "}
+            I'm able to operate some OS, such <a href="linux">Ubuntu</a>,{' '}
             <a href="windows">Windows 7, 8, 10</a> and <a href="osx">OSX</a>.
             I'm able to write documentation of code carefuly.
           </p>
